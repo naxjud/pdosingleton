ff8a8f0d4cf877a47557babc1b9a902dddd0a339
U-Moussa-PC\Moussa <naxjud@yahoo.com>
2015-12-10 17:31:56 +0100

    pdo version of the singlton class

--- classes/DB.php
+++ classes/DB.php
@@ -5,10 +5,11 @@
 */
 class DB
 {
-	private $_mysqli,
+	private $_pdoDBH,
 			$_query,
 			$_results=array(),
 			$_count =0;
+
 	public static $instance;
 
 	public static function getInstance()
@@ -21,19 +22,26 @@ class DB
 	}
 
 	public function __construct(){
-		$this->_mysqli = new mysqli('localhost','root','simsim', 'Login');
-		if($this->_mysqli->connect_error){
-			die($this->_mysqli->connect_error);
+		$this->_pdoDBH = new PDO("mysql:host=localhost;dbname=login", 'root', 'simsim');
+
+		try {
+		// assign PDO object to db variable
+			$this->_pdoDBH  = new PDO( 'mysql:host=localhost;dbname=login', 'root', 'simsim' );
+			$this->_pdoDBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+		}
+		catch (PDOException $e) {
+		//Output error - would normally log this to error file rather than output to user.
+			echo "Connection Error: " . $e->getMessage();
 		}
 	}
 
 	public function query($sql){
-		if($this->_query = $this->_mysqli->query($sql)){
-			while($row=$this->_query->fetch_object()){
+		if($this->_query = $this->_pdoDBH->query($sql)){
+			while($row=$this->_query->fetchObject()){
 				$this->_results[] = $row;
 			}
-			//num_rows Gets (in mysqli) the number of rows in a result
-			$this->_count = $this->_query->num_rows;
+			//num_rows Gets (in PDO) the number of rows in a result
+			$this->_count = $this->_query->rowCount();
 		}
 		return $this;
 	}
